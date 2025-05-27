5a5bc413e59e0f5552c1724b64221573e9624307
GustavoMSandoval <gmsandoval2005@gmail.com>
2025-05-27 09:27:00 -0300

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,13 +13,14 @@ export const Navbar = () => {
     <header>
         <nav className="z-[999] md:hidden">
             <div className="flex justify-end"><button className="ml-auto text-5xl" onClick={toggleMenu}>{isOpen ? <FiX /> : <FiAlignJustify />}</button></div>
-            { isOpen
-            && <ul>
-                <li><a href="#about">Sobre</a></li>
-                <li><a href="#technologies">Tecnologias</a></li>
-                <li><a href="#projects">Projetos</a></li>
-                <li><a href="#contacts">Contatos</a></li>
-            </ul>}
+            <div className={`overflow-hidden transition-all duration-500 ease-in-out ${isOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"}`}>
+                <ul>
+                    <li><a href="#about">Sobre</a></li>
+                    <li><a href="#technologies">Tecnologias</a></li>
+                    <li><a href="#projects">Projetos</a></li>
+                    <li><a href="#contacts">Contatos</a></li>
+                </ul>
+            </div>
         </nav>
         <nav>
             <ul className="hidden md:flex justify-center">

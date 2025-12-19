639ce69875dfe3ac7c4172240567554128f7527f
GustavoMSandoval <gmsandoval2005@gmail.com>
2025-12-19 12:37:59 -0300

    fix: Modificando about e refatorando algumas classes

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,12 +1,18 @@
 export const AboutSection = () => {
     return (
         <section id="about">
-                <div className="flex flex-col lg:flex-row justify-center items-center gap-6.5">
-                    <div>
-                        <h2>OI ME CHAMO GUSTAVO <br /> SOU UM <span>DESENVOLVEDOR</span> <br /> <span>WEB FULLSTACK</span></h2>
+                <div className="section-container flex flex-col-reverse lg:flex-row justify-center items-center gap-8">
+                    <div className="flex flex-col gap-7">
+                        <div>
+                            <h1>OI ME CHAMO GUSTAVO <br /> SOU UM <span>DESENVOLVEDOR</span> <br /> <span>WEB FULLSTACK</span></h1>
+                        </div>
+                        <div>
+                            <a href="#contacts" className="btn-primary">contatos</a>
+                        </div>
                     </div>
-                    <div className="w-[35vw]">
-                        <img className="rounded-full" src="/foto_bonita.jpg" alt="" />
+                    <div className="flex flex-col items-center gap-2">
+                        <img className="rounded-full mb-1" src="/foto_bonita.jpg" alt="" />
+                        <p>São Paulo, SP, Zona Sul</p>
                     </div>
                 </div>
         </section>

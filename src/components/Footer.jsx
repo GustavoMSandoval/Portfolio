425d8254589ffc4f1d27e5199a7fe4e98fb8ffc8
GustavoMSandoval <gmsandoval2005@gmail.com>
2025-12-19 14:26:06 -0300

    fix: Refatorando footer, correção de responsividade

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,33 @@
-import { FaLinkedin, FaGithub } from "react-icons/fa";
-import { FaWhatsapp } from "react-icons/fa6";
-
 export const Footer = () => {
     return (
-        <footer id="contacts">
-            <div>
-                <p>Tem um projeto em mente?</p>
-                <h1>Entre em <span>contato</span></h1>
-            </div>
-            <div>
-                <ul id="contact-links" className="text-1xl flex gap-1">
-                    <li>
-                        <a href="#">
-                            Linkedin
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            Whatsapp
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            Github
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <p>&#169; Criado por Gustavo Moreira Sandoval </p> 
+        <footer id="contacts" className="!py-24">
+            <div className="section-container">
+                <div className="!mb-4">
+                    <p>Tem um projeto em mente?</p>
+                    <h1>Entre em <span>contato</span></h1>
+                </div>
+                <div className="!mb-4">
+                    <ul id="contact-links" className="flex flex-col sm:flex-row">
+                        <li className="!px-1 lg:!pl-0 ">
+                            <a href="https://www.linkedin.com/in/gustavomsandoval/" target="_blank" className="btn-secondary">
+                                Linkedin
+                            </a>
+                        </li>
+                        <li className="!px-1 ">
+                            <a href="#" className="btn-secondary">
+                                Whatsapp
+                            </a>
+                        </li>
+                        <li className="!px-1 ">
+                            <a href="https://github.com/GustavoMSandoval" target="_blank" className="btn-secondary">
+                                Github
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <p>&#169; Criado por Gustavo Moreira Sandoval </p>
+                </div>
             </div>
         </footer>
     );

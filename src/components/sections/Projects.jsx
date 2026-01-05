96f53dc21e7b0e92b67df337844f0996434b1ece
GustavoMSandoval <gmsandoval2005@gmail.com>
2026-01-05 20:55:34 -0300

    fix: Arrumando link do vídeo

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -24,7 +24,7 @@ export default function ProjectsSection() {
         },
         {
             id: "3",
-            link: "https://youtu.be/YG12SAzl4m4?si=nrjDgbLYPVbVntS9",
+            link: "https://www.youtube.com/embed/YG12SAzl4m4",
             text: "Trabalho feito utilizando NextJS para uma empresa de comunicação visual o resultado <span>Google Analytics implementado</span> e <span> melhora de 87% no SEO</span>"
         },
         

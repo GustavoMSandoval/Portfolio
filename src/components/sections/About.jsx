18c21b3c41d4a09420abe15653e21d54fc882008
GustavoMSandoval <gmsandoval2005@gmail.com>
2026-01-05 21:24:48 -0300

    feat: Adicionando animação de digitação

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -3,8 +3,8 @@ export const AboutSection = () => {
         <section id="about" className="!my-24">
                 <div className="section-container flex flex-col-reverse lg:flex-row justify-center items-center gap-8">
                     <div className="flex flex-col gap-7">
-                        <div>
-                            <h1>OI ME CHAMO GUSTAVO <br /> SOU UM <span>DESENVOLVEDOR</span> <br /> <span>WEB FULLSTACK</span></h1>
+                        <div className="inline-block">
+                            <h1 className="typing">OI ME CHAMO GUSTAVO <br /> SOU UM <span className="primary-span">DESENVOLVEDOR</span> <br /> <span className="primary-span">WEB FULLSTACK</span></h1>
                         </div>
                         <div>
                             <a href="#contacts" className="btn-primary">contatos</a>

0b7f973890ea6c15fe7b3aba4d636d7d5510ab20
GustavoMSandoval <gmsandoval2005@gmail.com>
2026-01-13 20:17:01 -0300

    fix: Atualizando header e nav-links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
     }
 
   return (
-    <header>
+    <header className="fixed w-full bg-white top-0 z-50 shadow-2xs">
         <nav className="md:hidden">
             <div className="flex justify-end"><button className="ml-auto text-5xl" onClick={toggleMenu}>{isOpen ? <FiX /> : <FiAlignJustify />}</button></div>
             <div className={`overflow-hidden transition-all duration-500 ease-in-out ${isOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"}`}>
@@ -24,10 +24,10 @@ export const Navbar = () => {
         </nav>
         <nav>
             <ul className="hidden md:flex justify-center">
-                <li><a href="#about">Sobre</a></li>
-                <li><a href="#projects">Projetos</a></li>
-                <li><a href="#technologies">Tecnologias</a></li>
-                <li><a href="#contacts">Contatos</a></li>
+                <li><a className="nav-link" href="#about">Sobre</a></li>
+                <li><a className="nav-link" href="#projects">Projetos</a></li>
+                <li><a className="nav-link" href="#technologies">Tecnologias</a></li>
+                <li><a className="nav-link" href="#contacts">Contatos</a></li>
             </ul>
         </nav>
     </header>

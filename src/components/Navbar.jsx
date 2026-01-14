7e78b829c70b53616f271b49e2b90c234bacd8b5
GustavoMSandoval <gmsandoval2005@gmail.com>
2026-01-13 21:06:32 -0300

    fix: Modificando estilo do header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,8 @@ export const Navbar = () => {
     }
 
   return (
-    <header className="fixed w-full bg-white top-0 z-50 shadow-2xs">
-        <nav className="md:hidden">
+    <header className="fixed w-full top-0 md:top-1 z-50">
+        <nav className="md:hidden bg-white">
             <div className="flex justify-end"><button className="ml-auto text-5xl" onClick={toggleMenu}>{isOpen ? <FiX /> : <FiAlignJustify />}</button></div>
             <div className={`overflow-hidden transition-all duration-500 ease-in-out ${isOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"}`}>
                 <ul>
@@ -22,13 +22,22 @@ export const Navbar = () => {
                 </ul>
             </div>
         </nav>
-        <nav>
-            <ul className="hidden md:flex justify-center">
-                <li><a className="nav-link" href="#about">Sobre</a></li>
-                <li><a className="nav-link" href="#projects">Projetos</a></li>
-                <li><a className="nav-link" href="#technologies">Tecnologias</a></li>
-                <li><a className="nav-link" href="#contacts">Contatos</a></li>
-            </ul>
+          <nav className="flex justify-center">
+              <div className="nav-content bg-background/95 backdrop-blur-md shadow-[0_4px_12px_rgba(0,0,0,0.1)]">
+                <ul className="hidden md:flex justify-center items-center">
+                    <li className="nav-link-item flex gap-1">
+                        <img className="rounded-full w-12" src="/foto_bonita.jpg" alt="Minha foto" />
+                        <div className="flex flex-col">
+                            <span>Gustavo M Sandoval</span>
+                            <span className="text-secondary">São Paulo, SP, Zona Sul</span>
+                        </div>
+                    </li>
+                    <li className="nav-link-item"><a className="nav-link" href="#about">Sobre</a></li>
+                    <li className="nav-link-item"><a className="nav-link" href="#projects">Projetos</a></li>
+                    <li className="nav-link-item"><a className="nav-link" href="#technologies">Tecnologias</a></li>
+                    <li className="nav-link-item"><a className="nav-link" href="#contacts">Contatos</a></li>
+                </ul>
+            </div>
         </nav>
     </header>
   )

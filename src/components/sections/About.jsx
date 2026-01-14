253eed14a5eba961ba32770d650d677570a07861
GustavoMSandoval <gmsandoval2005@gmail.com>
2026-01-13 21:37:44 -0300

    fix: Atualizando sobre

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -3,16 +3,12 @@ export const AboutSection = () => {
         <section id="about" className="!my-24">
                 <div className="section-container flex flex-col-reverse lg:flex-row justify-center items-center gap-8">
                     <div className="flex flex-col gap-7">
-                        <div className="inline-block">
-                            <h1 className="typing">OI ME CHAMO GUSTAVO <br /> SOU UM <span className="primary-span">DESENVOLVEDOR</span> <br /> <span className="primary-span">WEB FULLSTACK</span></h1>
+                        <div className="inline-block text-center">
+                            <h1 className="typing">OI, ME CHAMO GUSTAVO <br /> SOU UM <br /> <span className="primary-span">DESENVOLVEDOR</span> <br className="block md:hidden"/> <span className="primary-span">WEB FULLSTACK</span></h1>
                         </div>
-                        <div>
+                    <div className="text-center">
                             <a href="#contacts" className="btn-primary">contatos</a>
                         </div>
-                    </div>
-                    <div className="flex flex-col items-center gap-2">
-                        <img className="rounded-full mb-1" src="/foto_bonita.jpg" alt="" />
-                        <p>São Paulo, SP, Zona Sul</p>
                     </div>
                 </div>
         </section>
